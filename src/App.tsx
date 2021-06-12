0fa662d05f4d407fd6a5236c7d72ba8382c49a08
Eric <ericen01@gmail.com>
2021-06-12 19:54:44 -0400

    fetch api with miragejs and axios and creating modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import { GlobalStyle } from './styles/global'
+import Modal  from 'react-modal'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
+import { useState } from 'react';
+import { NewTransactionsModal } from './components/NewTransactionsModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal = {handleOpenNewTransactionModal} />
       <Dashboard/>
+
+      <NewTransactionsModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+
+      />
       <GlobalStyle />
     </>
   );
---
+++ src/components/NewTransactionsModal/index.tsx
@@ -0,0 +1,17 @@
+import Modal from 'react-modal';
+import { Container } from './styles';
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionsModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    return (
+        <Modal 
+        isOpen={isOpen} 
+        onRequestClose={onRequestClose}>
+            <h2>Cadastrar transação</h2>
+        </Modal>
+    );
+}
