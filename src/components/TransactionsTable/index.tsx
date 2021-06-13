aeb10aa99a23275ba3323716781db7354b9866d6
Eric <ericen01@gmail.com>
2021-06-13 14:05:00 -0400

    finishing modal and validating form

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionsTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
 
     useEffect(() => {
         api.get('transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return (
@@ -22,19 +33,26 @@ export function TransactionsTable() {
                 </thead>
                 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12,000.00</td>
-                        <td>Dev</td>
-                        <td>20/02/2021</td>
+                  {transactions.map(transaction =>{
+                      return (
+                    <tr key= {transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>
+                        {new Intl.NumberFormat('pt-BR',{
+                            style: 'currency',
+                            currency: 'BRL',
+                        }).format(transaction.amount)
+                        }
+                        </td>
+                        <td>{transaction.category}</td>
+                        <td>
+                        {new Intl.DateTimeFormat('pt-BR',{
+                        }).format(new Date(transaction.createdAt))
+                        }</td>
                     </tr>
 
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">R$ -1,000.00</td>
-                        <td>Casa</td>
-                        <td>16/02/2021</td>
-                    </tr>
+                      )
+                  })}
 
                    
                 </tbody>
--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -21,10 +21,15 @@ export const Container = styled.div`
             border: 0;
             background: var(--shape);
             color: var(--text-body);
-            border-radius: 0.25rem;
+
 
             &:first-child {
                 color: var(--text-title);
+                border-radius: 0.25rem 0 0 0.25rem;
+            }
+
+            &:last-child {
+                border-radius: 0 0.25rem 0.25rem 0
             }
 
             &.deposit{
